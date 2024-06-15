16faeee490dc3774a2044255279ca852fee897a8
Emil <qirimtatarbala@gmail.com>
2024-06-16 02:53:23 +0300

    connect fix

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+app.get('/', function(request, respons) {
+	respons.sendFile(__dirname + '/index.html');
