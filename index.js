660626ee293856bb05ce6a62ee6567f4f9c911ad
Emil <qirimtatarbala@gmail.com>
2024-06-16 03:02:06 +0300

    fix bags in socket

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-server.listen(3000);
+http.listen(3000, function() {
+  console.log('Server listening on *:3000');
+});
 
-app.get('/', function(request, respons) {
-	respons.sendFile(__dirname + '/index.html');
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
