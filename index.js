b2e396dd941e06b811bbd5c3171a1561400e9524
Emil <qirimtatarbala@gmail.com>
2024-06-16 03:11:36 +0300

    conection

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ http.listen(3000, function() {
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
+connections = [];
+
+io.sockets.on('connection', function(socket) {
+	console.log("Успешное соединение");
+	
+	connections.push(socket);
+
+	
+	socket.on('disconnect', function(data) {
+		connections.splice(connections.indexOf(socket), 1);
+		console.log("Отключились");
+	});
+
+	
+	socket.on('send mess', function(data) {
+		io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+	});
+
+});
